7806412ebc797561d4865822e1a01358daafc09e
Hoangdev <nvhhoang286@gmail.com>
2024-04-10 07:23:31 +0700

    first

---
+++ RESTfulAPIDemo/DBContext/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RESTfulAPIDemo.Models;
+using System.Collections.Generic;
+
+namespace RESTfulAPIDemo.DBContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+  : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
